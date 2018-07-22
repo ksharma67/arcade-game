c77c75559d5469ba6898cb0c04b7e76656787235
Kshitij Sharma <32421161+ksharma67@users.noreply.github.com>
2018-07-22 20:38:00 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -381,20 +381,3 @@ function gameStop() {
     removeCollectiblesFromCanvas();
     enableCharacterSelection();
 }
-// lock scroll position, but retain settings for later
-var scrollPosition = [
-  self.pageXOffset || document.documentElement.scrollLeft || document.body.scrollLeft,
-  self.pageYOffset || document.documentElement.scrollTop  || document.body.scrollTop
-];
-var html = jQuery('html'); // it would make more sense to apply this to body, but IE7 won't have that
-html.data('scroll-position', scrollPosition);
-html.data('previous-overflow', html.css('overflow'));
-html.css('overflow', 'hidden');
-window.scrollTo(scrollPosition[0], scrollPosition[1]);
-
-
-// un-lock scroll position
-var html = jQuery('html');
-var scrollPosition = html.data('scroll-position');
-html.css('overflow', html.data('previous-overflow'));
-window.scrollTo(scrollPosition[0], scrollPosition[1])
